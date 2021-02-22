8b9006825dc90c8408bfe7413676fd7da2244cb9
Elfenbug <elfenbug@gmail.com>
2021-02-22 16:21:03 +0500

    First commit

---
+++ Lesson_01/Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+    public static void main(String[] args) {
+        byte core = 4;
+        short ram = 16;
+        int hdd = 512;
+        long windowsVersion = 10;
+        float processorMin = 1.6f;
+        double processorMax = 2.11;
+        char anyCharParameter = 'Z';
+        boolean touchScreen = false;
+        
+        System.out.println(core);
+        System.out.println(ram);
+        System.out.println(hdd);
+        System.out.println(windowsVersion);
+        System.out.println(processorMin);
+        System.out.println(processorMax);
+        System.out.println(anyCharParameter);
+        System.out.println(touchScreen);
+    }
+}
