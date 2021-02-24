d0c483cb792aca75ed0b072b72b0c42b98b478a1
Elfenbug <elfenbug@gmail.com>
2021-02-24 20:48:34 +0500

    коммит дз п.11 после первой правки

--- Lesson_01/Calculator.java
+++ Lesson_01/Calculator.java
@@ -2,23 +2,23 @@ public class Calculator {
     public static void main(String[] args) {
         int firstInputNumber = 30;
         int secondInputNumber = 6;
-        char selectOper = '+';
+        char mathSign = '+';
 
-        if(selectOper == '+' ) {
+        if(mathSign == '+' ) {
             System.out.println(firstInputNumber + secondInputNumber);
-        } else if(selectOper == '-') {
+        } else if(mathSign == '-') {
             System.out.println(firstInputNumber - secondInputNumber);
-        } else if(selectOper == '*') {
+        } else if(mathSign == '*') {
             System.out.println(firstInputNumber * secondInputNumber);
-        } else if(selectOper == '/') {
+        } else if(mathSign == '/') {
             System.out.println(firstInputNumber / secondInputNumber);
-        } else if(selectOper == '^') {
-            int counter = firstInputNumber;
-                for(int i = 1; i < secondInputNumber; i++) {
-                    firstInputNumber*=counter;
-                }
+        } else if(mathSign == '^') {
+            int result = firstInputNumber;
+            for(int i = 1; i < secondInputNumber; i++) {
+                firstInputNumber *= result;
+            }
             System.out.println(firstInputNumber);
-        } else if(selectOper == '%') {
+        } else if(mathSign == '%') {
             System.out.println(firstInputNumber % secondInputNumber);
         }
     }
--- Lesson_01/GuessNumber.java
+++ Lesson_01/GuessNumber.java
@@ -1,21 +1,18 @@
 public class GuessNumber {
     public static void main(String[] args) {
         int compNumber = 30;
-        int userNumber = 2;
+        int userNumber = 30;
 
-        if(userNumber != compNumber) {
-            while(userNumber != compNumber) {
-                if(userNumber == compNumber) {
-                    System.out.println("Вы угадали!");
-                } else if(userNumber > compNumber) {
-                    System.out.println("Введенное вами число больше того, что загадал компьютер");
-                    userNumber -= 1;
-                } else if(userNumber < compNumber) {
-                    System.out.println("Введенное вами число меньше того, что загадал компьютер");
-                    userNumber += 1;
-                }
+        while(userNumber != compNumber) {
+            if(userNumber > compNumber) {
+                System.out.println("Введенное вами число больше того, что загадал компьютер");
+                userNumber--;
+            } else if(userNumber < compNumber) {
+                System.out.println("Введенное вами число меньше того, что загадал компьютер");
+                userNumber++;
             }
         }
+
         if(userNumber == compNumber) {
             System.out.println("Вы угадали!");
         }
