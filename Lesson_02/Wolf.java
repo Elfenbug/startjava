f20a5b79e4f5784b526c1db76fed51428638d11e
Elfenbug <elfenbug@gmail.com>
2021-02-25 23:49:11 +0500

    п.5 (модифицированный п.3 с геттерами и сеттерами)

--- Lesson_02/Wolf.java
+++ Lesson_02/Wolf.java
@@ -1,29 +1,71 @@
 public class Wolf {
-    String gender;
-    String name;
-    double weight;
-    int age;
-    String color;
+    private String gender;
+    private String name;
+    private double weight;
+    private int age;
+    private String color;
 
-    void go() {
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void go() {
 
     }
 
-    void sit() {
+    public void sit() {
 
     }
 
-    void run() {
+    public void run() {
 
     }
 
-    void howl() {
+    public void howl() {
 
     }
 
-    void hunt() {
+    public void hunt() {
 
     }
-
-    
 }
--- Lesson_02/WolfTest.java
+++ Lesson_02/WolfTest.java
@@ -1,23 +1,22 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf wolfOne = new Wolf();
-        wolfOne.gender = "Female";
-        wolfOne.name = "Jack";
-        wolfOne.weight = 70.0;
-        wolfOne.age = 6;
-        wolfOne.color = "Black";
+        wolfOne.setGender("Female");
+        wolfOne.setName("Jack");
+        wolfOne.setWeight(70.0);
+        wolfOne.setAge(6);
+        wolfOne.setColor("Black");
 
-        System.out.println("Gender is " + wolfOne.gender);
-        System.out.println("Name is " + wolfOne.name);
-        System.out.println("Weight is " + wolfOne.weight);
-        System.out.println("Age is " + wolfOne.age);
-        System.out.println("Color is " + wolfOne.color);
+        System.out.println("Gender is " + wolfOne.getGender());
+        System.out.println("Name is " + wolfOne.getName());
+        System.out.println("Weight is " + wolfOne.getWeight());
+        System.out.println("Age is " + wolfOne.getAge());
+        System.out.println("Color is " + wolfOne.getColor());
 
         wolfOne.go();
         wolfOne.sit();
         wolfOne.run();
         wolfOne.howl();
         wolfOne.hunt();
-
     }
 }
