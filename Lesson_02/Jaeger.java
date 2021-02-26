8b1d8dc1ac3f214048eb7e1525fd123e061e70f5
Elfenbug <elfenbug@gmail.com>
2021-02-27 00:24:50 +0500

    Jaeger с сеттерами

---
+++ Lesson_02/Jaeger.java
@@ -0,0 +1,99 @@
+public class Jaeger {
+    private String modelName;
+    private String mark;
+    private String origin;
+    private float height;
+    private float weight;
+    private int speed;
+    private int strength;
+    private int armor;
+    private String opSystem;
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getStrength() {
+        return speed;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public String getOpSystem() {
+        return opSystem;
+    }
+
+    public void setOpSystem(String opSystem) {
+        this.opSystem = opSystem;
+    }
+
+    public boolean drift() {
+        return true;
+    }
+
+    public void move() {
+    }
+
+    public String scanKaiju() {
+        return "Сканирование завершено";
+    }
+
+    public void useWeapon() {
+
+    }
+
+}
---
+++ Lesson_02/JaegerTest.java
@@ -0,0 +1,33 @@
+public class JaegerTest {
+    public static void main(String args[]) {
+        Jaeger gipsyDanger = new Jaeger();
+        Jaeger chernoAlpha = new Jaeger();
+
+        gipsyDanger.setModelName("Gipsy Dander");
+        gipsyDanger.setMark("Mark-3");
+        gipsyDanger.setOrigin("USA");
+        gipsyDanger.setHeight(79.25f);
+        gipsyDanger.setWeight(1.98f);
+        gipsyDanger.setSpeed(7);
+        gipsyDanger.setStrength(8);
+        gipsyDanger.setArmor(6);
+        gipsyDanger.setOpSystem("Blue Spark 4.1");
+
+        chernoAlpha.setModelName("Cherno Alpha");
+        chernoAlpha.setMark("Mark-1");
+        chernoAlpha.setOrigin("Russia");
+        chernoAlpha.setHeight(85.34f);
+        chernoAlpha.setWeight(2.412f);
+        chernoAlpha.setSpeed(3);
+        chernoAlpha.setStrength(10);
+        chernoAlpha.setArmor(10);
+        chernoAlpha.setOpSystem("Unknown");
+
+        System.out.println(gipsyDanger.getOpSystem());
+        System.out.println(chernoAlpha.getHeight());
+
+        gipsyDanger.move();
+        chernoAlpha.useWeapon();
+
+    }
+}
